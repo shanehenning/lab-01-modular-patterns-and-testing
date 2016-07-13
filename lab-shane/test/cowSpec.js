f080063ce22520792652a5fb35f423a6c8d3fe5c
Shane Henning <shanerhenning@gmail.com>
2016-07-13 11:32:33 -0700

    gosh, this is the most fun game ever!

--- lab-shane/test/cowSpec.js
+++ lab-shane/test/cowSpec.js
@@ -3,6 +3,6 @@ const cow = require('../lib/cow');
 
 describe('the cow function', function(){
   it('should have a cow say a passed in parameter', function(){
-    expect(cow('hi')).to.eql('cow hi');
+    expect(cow('hi')).to.eql('the cow says hi');
   });
 });
